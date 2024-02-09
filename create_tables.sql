b0a961544d32f092b7a1f0d62e14fdb9de8726b9
Jens-Uwe Mager <jum@anubis.han.de>
2024-02-09 14:00:13 -0500

    Add an index for the tokens of the update table, normalize sql case.

--- create_tables.sql
+++ create_tables.sql
@@ -1,15 +1,16 @@
 .read settings.sql
 BEGIN;
-drop table if exists updates;
-drop table if exists hosts;
-drop trigger if exists hosts_update;
-drop trigger if exists updates_update;
+DROP INDEX IF EXISTS updates_token_index;
+DROP TABLE IF EXISTS updates;
+DROP TABLE IF EXISTS hosts;
+DROP TRIGGER IF EXISTS hosts_update;
+DROP TRIGGER IF EXISTS updates_update;
 
-create table hosts (
+CREATE TABLE hosts (
 	token CHAR(43) PRIMARY KEY,
 	name VARCHAR(255) NOT NULL,
-	ip4addr varchar(255),
-	ip6addr varchar(255),
+	ip4addr VARCHAR(255),
+	ip6addr VARCHAR(255),
 	modified DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -17,10 +18,10 @@ create table hosts (
 CREATE TRIGGER hosts_update AFTER UPDATE ON hosts
 	FOR EACH ROW WHEN OLD.modified != DATETIME()
 BEGIN
-	UPDATE hosts SET modified = DATETIME() where token = NEW.token;
+	UPDATE hosts SET modified = DATETIME() WHERE token = NEW.token;
 END;
 
-create table updates (
+CREATE TABLE updates (
 	id INTEGER NOT NULL PRIMARY KEY,
 	token CHAR(43) NOT NULL,
 	cmd VARCHAR(255) NOT NULL,
@@ -28,12 +29,15 @@ create table updates (
 	modified DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	FOREIGN KEY(token) REFERENCES hosts(token)
+	ON DELETE CASCADE
+	ON UPDATE RESTRICT
 );
+CREATE INDEX updates_token_index ON updates (token);
 
 CREATE TRIGGER updates_update AFTER UPDATE ON updates
 	FOR EACH ROW WHEN OLD.modified != DATETIME()
 BEGIN
-	UPDATE updates SET modified = DATETIME() where id = NEW.id;
+	UPDATE updates SET modified = DATETIME() WHERE id = NEW.id;
 END;
 
 COMMIT;
